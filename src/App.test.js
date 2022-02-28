d08e6a0a1b605b5bc78d5371984fc2dd539bd0a7
Zak Horton <zhorton999@gmail.com>
2022-02-27 21:01:04 -0800

    changes

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello from V2/i);
+  const linkElement = screen.getByText(/We now have Auth!/i);
   expect(linkElement).toBeInTheDocument();
 });
